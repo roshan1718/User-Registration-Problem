2dc0d18fae969774b02d7a07df4f74aaefd6ac68
roshan1718 <roshanshinde1717@gmail.com>
2020-03-31 19:10:31 +0530

    ADDED code to email validation

--- src/main/java/com/bridgelabz/userRegistration/UserRegistration.java
+++ src/main/java/com/bridgelabz/userRegistration/UserRegistration.java
@@ -5,6 +5,7 @@ import java.util.regex.Pattern;
 public class UserRegistration {
 
     String namePattern="^[A-Z][a-zA-Z]{2,}$";
+    String emailPattern="^[a-zA-Z]+([+]?[-]?[.]?[_]?[a-zA-Z0-9]+)?[@]{1}[a-z]+[.]{1}[a-z]{2,}([.]?[a-z]+)?$";
 
     public static void main(String[] args) {
         System.out.println("Welcome To User Registration");
@@ -12,4 +13,11 @@ public class UserRegistration {
     public boolean checkName(String name) {
         return Pattern.matches(namePattern,name);
     }
+
+    public boolean checkEmail(String email) {
+        return Pattern.matches(emailPattern,email);
+    }
+
+
+
 }
--- src/test/java/com/bridgelabz/userRegistrationTest/UserRegistrationTest.java
+++ src/test/java/com/bridgelabz/userRegistrationTest/UserRegistrationTest.java
@@ -27,4 +27,27 @@ public class UserRegistrationTest {
         Assert.assertFalse(lastName);
     }
 
+    @Test
+    public void email_ValidThenTrue() {
+        String validEmails[]={"abc.xyz@bl.co.in","abc@yahoo.com","abc-100@yahoo.com","abc.100@yahoo.com",
+                "abc111@abc.com","abc-100@abc.net","abc.100@abc.com.au",
+                "abc@gmail.com.com","abc+100@gmail.com"};
+        for(int index=0;index<validEmails.length;index++){
+            boolean email=test.checkEmail(validEmails[index]);
+            Assert.assertTrue(email);
+        }
+    }
+    @Test
+    public void email_InvalidThenFalse() {
+        String invalidEmails[]={"abc","abc()*@gmail.com","abc@.com.my","abc123@gmail.a",
+                "abc123@.com","abc123@.com.com",".abc@abc.com","abc()*@gmail.com",
+                "abc@%*.com","abc..2002@gmail.com","abc.@gmail.com","abc@abc@gmail.com",
+                "abc@gmail.com.1a","abc@gmail.com.aa.au"};
+        for(int index1=0;index1<invalidEmails.length;index1++) {
+            boolean Email = test.checkEmail(invalidEmails[index1]);
+            Assert.assertFalse(Email);
+        }
+    }
+
+
 }
